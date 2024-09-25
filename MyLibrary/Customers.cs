af55a86bde41c90c5179f821160d311b3cbf7758
David Cortina <davidcortinaflores@gmail.com>
2024-09-25 11:02:03 +0200

    Ejemplo con la landa esa chunga

--- MyLibrary/Customers.cs
+++ MyLibrary/Customers.cs
@@ -2,5 +2,8 @@
 {
     internal class Customers
     {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string FullName => $"{FirstName} {LastName}";
     }
 }
